e11fa8fc770aea3842fcf469b75e5ea694b93007
Toshinori Sumida <alias.ceuta@gmail.com>
2022-07-10 23:24:48 +0900

    laravel debugger

--- resources/js/Pages/ShowComponents/Sample1.jsx
+++ resources/js/Pages/ShowComponents/Sample1.jsx
@@ -3,7 +3,7 @@ import Authenticated from '@/Layouts/Authenticated'
 import { Head } from '@inertiajs/inertia-react'
 
 const Sample1 = (props) => {
-  return <div>Sample1</div>
+  return <div>{props.users}</div>
 }
 
 export default Sample1
