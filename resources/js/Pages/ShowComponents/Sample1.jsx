3a95947e6a0a1eedc8571c7e8739991550f7f54d
Toshinori Sumida <alias.ceuta@gmail.com>
2022-07-11 02:44:48 +0900

    multi login create jsx file

--- resources/js/Pages/ShowComponents/Sample1.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Authenticated from '@/Layouts/Authenticated'
-import { Head } from '@inertiajs/inertia-react'
-
-const Sample1 = (props) => {
-  return <div>{props.users}</div>
-}
-
-export default Sample1
